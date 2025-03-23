3213061765cd762a4eab3281f21cb1aed9864be5
shibatanaoto <naoto.shibata510@gmail.com>
2025-03-23 20:55:15 +0900

    update main.py

--- main.py
+++ main.py
@@ -2,12 +2,8 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-# Markdownで記述
-st.markdown("""
-# Hello world!!!
-""")
+st.markdown("# Hello world!!!")
 
-# グラフを書く
 df: pd.DataFrame = pd.DataFrame(
     np.random.randn(30, 3),
     columns=["x", "y", "z"]
